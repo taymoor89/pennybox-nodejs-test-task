8da19f96c3e427779acddf0db25fefec68339a83
Taimoor Aslam <taymooraslam@gmail.com>
2017-07-17 22:34:00 +0500

    refactor(user.controller.getList): async/await version of getList
    - Support pagination with limit and skip query pramaters
    - Cover getList() with tests

--- src/api/user/user.controller.js
+++ src/api/user/user.controller.js
@@ -2,15 +2,32 @@ const UserModel = require('./user.model');
 
 class UserController {
 
-  static getList (req, res) {
-    // TODO: TDD me & Refactor me
-    UserModel
-      .find({}, (err, users) => {
-        if (err) {
-          return console.error(err);
+  static async getList (req, res, next) {
+    let {limit, skip} = req.query;
+    limit = +limit || 10; //parse limit to number if exists otherwise make it default to 10 
+    skip  = +skip || 0; //parse skip to number if exists otherwise make it default to 0
+
+    try {
+      let total = await UserModel.count({});
+      let users = await UserModel
+        .find({})
+        .skip(skip)
+        .limit(limit)
+        .exec();
+
+      let headers = {
+        amount: users.length,
+        total
+      };
+
+      res
+        .set(headers)
+        .status(200)
+        .json(users);
+    }
+    catch (error) {
+      next(error);
         }
-        res.json(users)
-      })
   }
 
   static create (req, res) {
---
+++ src/api/user/user.controller.spec.js
@@ -0,0 +1,91 @@
+let _         = require('lodash');
+let Mongoose  = require('mongoose');
+let request   = require('supertest');
+let chai      = require('chai');
+let App       = require('../../app');
+let UserModel = require('./user.model');
+let Config    = require('../../config/environment');
+let expect    = chai.expect;
+let assert    = chai.assert;
+
+describe('user.controller', () => {
+  let app = null;
+  
+  before(async () => {
+    app = new App().app;
+    await Mongoose.connect(Config.mongo.uri, Config.mongo.options);
+    await UserModel.remove({}); // Remove previously added users in test db
+  });
+
+  describe('[GET] /users', () => {
+    let users = null;
+
+    beforeEach(async () => {
+      docs = [
+        {
+          name: 'Anton',
+          role: 'admin'
+        },
+        {
+          name: 'Taimoor',
+          role: 'developer'
+        },
+        {
+          name: 'Tim Cook',
+          role: 'product_owner'
+        }
+      ];
+      users = await UserModel.create(docs);
+    });
+
+    afterEach(async () => {
+      let ids   = _.map(users, '_id');
+      let query = {
+        _id: {
+          $in: ids
+        }
+      };
+      await UserModel.remove(query);
+    });
+
+    it('Should get users', () => {
+      return request(app)
+        .get('/users')
+        .expect(200)
+        .expect((response) => {
+          let {body, header} = response;
+          expect(body.length).to.be.equal(3);
+          expect(header.amount).to.be.equal('3');
+          expect(header.total).to.be.equal('3');
+        });
+    });
+
+    it('should limit records with limit query parameter', () => {
+      const limit = 1;
+      return request(app)
+        .get('/users')
+        .query({limit})
+        .expect(200)
+        .expect((response) => {
+          let {body, header} = response;
+          expect(body.length).to.be.equal(1);
+          expect(header.amount).to.be.equal('1');
+          expect(header.total).to.be.equal('3');
+        });
+    });
+
+    it('should skip records with skip query parameter', () => {
+      const skip  = 1;
+      return request(app)
+        .get('/users')
+        .query({skip})
+        .expect(200)
+        .expect((response) => {
+          let {body, header} = response;
+          expect(body.length).to.be.equal(2);
+          expect(header.amount).to.be.equal('2');
+          expect(header.total).to.be.equal('3');
+        });
+    });
+  });
+});
